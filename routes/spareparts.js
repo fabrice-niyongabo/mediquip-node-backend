f170bd4ca8d0bffd16c802ff26646596e5083b93
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-01-05 03:39:44 +0200

    editing spare parts

--- routes/spareparts.js
+++ routes/spareparts.js
@@ -82,4 +82,41 @@ router.post("/", auth, async (req, res) => {
   }
 });
 
+router.put("/", auth, async (req, res) => {
+  try {
+    const { name, model, price, id } = req.body;
+    // Validate user input
+    if (!(name && model && price && id)) {
+      res.status(400).send({
+        status: "Error",
+        msg: "Provide correct info",
+      });
+    }
+    // check if device already exist
+
+    const device = await SparePrarts.updateOne(
+      {
+        _id: id,
+      },
+      { name, model, price }
+    );
+    if (device) {
+      return res.status(201).json({
+        status: "success",
+        msg: "Spare part updated successfull!",
+        device,
+      });
+    } else {
+      return res.status(400).json({
+        status: "Error",
+        msg: "Something went wrong, try again later",
+      });
+    }
+  } catch (err) {
+    res.status(400).send({
+      msg: err.message,
+    });
+  }
+});
+
 module.exports = router;
