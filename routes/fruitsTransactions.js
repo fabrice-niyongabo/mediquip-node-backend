652a6c8252e3af44db1a26e8b6ac6e6e2c9c2cfa
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-01-16 22:01:20 +0200

    transactions for fruits

---
+++ routes/fruitsTransactions.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+
+const Transactions = require("../model/fruitsTransactions");
+
+router.get("/", async (req, res) => {
+  try {
+    const transactions = await Transactions.find({});
+    return res.status(201).json({
+      status: "success",
+      transactions,
+    });
+  } catch (err) {
+    res.status(400).send({
+      msg: err.message,
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const transaction = await Transactions.create(req.body);
+    return res.status(201).json({
+      status: "success",
+      transaction,
+    });
+  } catch (err) {
+    res.status(400).send({
+      msg: err.message,
+    });
+  }
+});
+
+module.exports = router;
